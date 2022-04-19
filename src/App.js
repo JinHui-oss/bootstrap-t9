037f09aa52d16bbb2df1a65fb1229fedc57aaf81
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-19 17:16:22 +0800

    added navbar and new file for staff navbar

--- src/App.js
+++ src/App.js
@@ -4,27 +4,42 @@ import Signup from "./Login/Signup";
 import Home from "./General/Home";
 import Signin from "./Login/Signin";
 import Profile from "./Staff/Profile";
-import Error from "./Error";
+import Error from "./Message/Error";
 import { AuthContextProvider } from "./Scripts/authContext";
 import ProtectedRoute from "./ProtectedRoute";
+import { getAuth } from 'firebase/auth'
+import General from "./Navbar/General";
 
 
 function App() {
-  return (
+  
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+    if (user) {
+    return(
+      <div className="App">
+        <General />
+      </div>
+    )
+    }else {
+   
+    return(
     <div className="App">
-      <AuthContextProvider>
+      <General />
+        <AuthContextProvider>
         <Routes>
           <Route path ='/' element={<Home/>} />
           <Route path="/signin" element={<Signin/>} />
           <Route path='/signup' element={<Signup/>} />
           <Route path='/account' element={<ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>} />
-        <Route path='*' element={<Error/>} />
-      </Routes>
-    </AuthContextProvider>
-    </div>
-  );
+          <Profile /> </ProtectedRoute>} />
+          <Route path='*' element={<Error/>} />
+        </Routes>
+        </AuthContextProvider>  
+      </div>
+    );
+  }
 }
 
 export default App;
