326ef6fffe7435cc3ced4240190b441fae2b34d0
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-20 15:20:13 +0800

    reorganize the code , added comment and change the redirect.

--- src/App.js
+++ src/App.js
@@ -1,65 +1,85 @@
+// React 
 import React from "react";
 import { Route, Routes } from 'react-router-dom';
-import Signup from "./Login/Signup";
-import Home from "./General/Index";
-import Signin from "./Login/Signin";
-import Profile from "./Staff/Account/Profile";
-import Index from "./Staff/Index";
-import Error from "./General/Message/Error";
+
+// Firebase Database
 import { AuthContextProvider } from "./Scripts/authContext";
-import ProtectedRoute from "./ProtectedRoute";
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import ProtectedRoute from "./ProtectedRoute";
 
 // Navbar
 import General from "./Navbar/General";
 import Staff from "./Navbar/Staff";
 
+// General Page
+import Home from "./General/Index";
+import Error from "./General/Message/Error";
+
+// Account Signin & out inteface
+import Signin from "./Login/Signin";
+import Signup from "./Login/Signup";
+
+// Staff Interface
+import Profile from "./Staff/Account/Profile";
+import Index from "./Staff/Index";
+
+// Staff & Kit Interface
+import Kit from "./Staff/Kit/Index";
+
+
 
 function App() {
+
+    // get data from the firebase/auth
+    const auth = getAuth();
   
-  const auth = getAuth();
-  const user = auth.currentUser;
-  //console.log(user);
+    // check the correct display output
+    // console.log(user);
  
-  onAuthStateChanged(auth, (currentUser) => {
+    onAuthStateChanged(auth, (currentUser) => {
     if (currentUser) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-     console.log('yes');
-     
-      // ...
+      // check the correct display output if condition is met 
+      console.log('yes');
+      //
     } else {
-      // User is signed out
-      // ...
+      // check the incorrect display output if condition is not met
       console.log('no');
     
     }
   })
-   
-      return(
-        <div className="App">
-            <AuthContextProvider>
-            <Routes>
-              
-              <Route path ='/' element={<Home/>} />
-              <Route path="/signin" element={<Signin/>} />
-              <Route path='/signup' element={<Signup/>} />
-              
-              <Route path='/account' element={<ProtectedRoute>
-              <Staff />
-              <Profile />  
-              </ProtectedRoute>} />
-
-              <Route path='/dashboard' element={<ProtectedRoute>
-              <Staff />
-              <Index />
-              </ProtectedRoute>} />
-
-              <Route path='*' element={<Error/>} />
-            </Routes>
-            </AuthContextProvider>  
-          </div>
-        );
-    }
+  return(
+    <div className="App">
+      <AuthContextProvider>
+      <Routes>
+         {/* A JSX comment */}      
+        <Route path ='/' element={<Home/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path='/signup' element={<Signup/>} />
+        
+         {/* A JSX comment */}      
+        <Route path='/account' element={<ProtectedRoute>
+          <Staff />
+          <Profile />  
+        </ProtectedRoute>} />
+        
+         {/* A JSX comment */}
+        <Route path='/dashboard' element={<ProtectedRoute>
+          <Staff />
+          <Index />
+        </ProtectedRoute>} />
+        
+         {/* A JSX comment */}
+        <Route path='/Kit' element={<ProtectedRoute>
+          <Staff />
+          <Kit />
+        </ProtectedRoute>} />
+        
+        {/* A JSX comment */}
+        <Route path='*' element={<Error/>} />
+        </Routes>
+      </AuthContextProvider>  
+    </div>
+  );
+}
 
 export default App;
