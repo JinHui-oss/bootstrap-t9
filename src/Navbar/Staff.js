a0f6c403ee25bd427610c843662e74744392b591
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-20 14:12:24 +0800

    added navbar for staff

--- src/Navbar/Staff.js
+++ src/Navbar/Staff.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import {
+    Nav,
+    Navbar,
+    Container,
+}from 'react-bootstrap'
+
+import { useNavigate } from 'react-router-dom';
+import { UserAuth } from "../Scripts/authContext" 
+
 
 function Staff() {
+  const { logout } = UserAuth();
+  const naviagte = useNavigate();
+
+  const handlelogout= async() =>{
+    try{
+        await logout()
+        naviagte('/signin')
+        console.log('you are logged out');
+    }catch(e){
+        console.log(e.message)
+    }
+  }
   return (
-    <div>
-      home
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="/dashboard">YECC</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
+        <Nav.Link href="/kit">Kit</Nav.Link>
+        <Nav.Link href="/Account">Account</Nav.Link>
+        <Nav.Link href="/report">Report</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link href="#deets">Notifications</Nav.Link>
+        <Nav.Link href="#deets">Announcement</Nav.Link>
+        <Nav.Link onClick={handlelogout} >LogOut</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
