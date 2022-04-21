fdf4e74650978764636679814794254f1b7ed936
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-21 13:37:45 +0800

    fix the error of not following the jsx format

--- src/Staff/Kit/AddKit.jsx
+++ src/Staff/Kit/AddKit.jsx
@@ -12,28 +12,28 @@ function AddKit() {
         <h2>Kit Infomation</h2>
         <hr />
       <form>
-        <div class="form-group">
-          <label for="KitName">Name </label>
-          <input type="text" class="form-control" id="KitName" placeholder="Dementia Kit xx - example" required />
+        <div className="form-group">
+          <label htmlFor='KitName'>Name </label>
+          <input type="text" className="form-control" id="KitName" placeholder="Dementia Kit xx - example" required />
         </div>
-        <div class="form-group">
-          <label for="KitQuantity">Quantity </label>
-          <input type="number" class="form-control" id="KitQuantity" placeholder="1" required />
+        <div className="form-group">
+          <label htmlFor="KitQuantity">Quantity </label>
+          <input type="number" className="form-control" id="KitQuantity" placeholder="1" required />
         </div>
-        <div class="form-group">
-          <label for="KitDescription">Description</label>
-          <textarea class="form-control" id="KitDescription" rows="3" placeholder='Kit Content' required ></textarea>
+        <div className="form-group">
+          <label htmlFor="KitDescription">Description</label>
+          <textarea className="form-control" id="KitDescription" rows="3" placeholder='Kit Content' required ></textarea>
         </div>
-        <form>
-        <div class="form-group">
-          <label for="KitPictures">Kit Pictures</label>
+       
+        <div className ="form-group">
+          <label htmlFor="KitPictures">Kit Pictures</label>
           <br />
-          <input type="file" class="form-control-file" id="KitPictures" />
+          <input type="file" className="form-control-file" id="KitPictures" />
         </div>
-        <Button onclick={handlesubmit} className='submit'>Submit</Button>
+        <Button onClick={handlesubmit} className='submit'>Submit</Button>
         <br />
         <Button href='/kit' className='submit'>Back</Button>
-        </form>
+        
       </form>
     </div>
   )
