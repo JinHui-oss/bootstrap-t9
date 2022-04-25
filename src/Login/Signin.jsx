ff83e4dd79155bfa89f9f0a570e6fd9733f18e57
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-25 13:28:48 +0800

    change from tailwinds to bootstrap

--- src/Login/Signin.jsx
+++ src/Login/Signin.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Link, useNavigate, } from 'react-router-dom'
 import { UserAuth } from '../Scripts/authContext'
+import { Form, Button } from 'react-bootstrap'
 
 
 function Signin() {
@@ -23,28 +24,25 @@ function Signin() {
     }
 
     return (
-    <div className='max-w-[700px] mx-auto my-16 p-4'>
-    <h1 className='text-center text-3xl font-bold'> Sign in to your account </h1>
+    <div className='header'>
+    <h1 className='header-title'> Sign in to your account </h1>
     <br />
     <p> Don't have an account yet? <Link to ='/signup' className='underline'>Sign up.</Link></p>
   
-  <form onSubmit={handleSubmit}>
-      <div className='flex flex-col py-2'>
-          <label className='py-2 font-medium'>Email Address</label>
-          <br />
-          <input onChange={(e) => setEmail(e.target.value)} className='border p-3' type="email" />
-      </div>
-      <div className ='flex flex-col py-2'>
-          <label className='py-2 font-medium' >Password</label>
-          <br />
-          <input onChange={(e) => setPassword(e.target.value)} className= 'border p-3' type="password" />
-      </div>
-      <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
-      Sign In</button>
-      <br />
-  </form>
-  </div>
-)
-}
+    <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+    </Form.Group>
+    
+    <Button variant="primary" type="submit"> Submit </Button>
+    </Form>
+</div>
+)}
 
 export default Signin
