bb3feee4c819b64de4a0eb43a8ea83bdcaefabdb
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-25 17:30:07 +0800

    create, retrieve the data from the database and display to the user.

--- src/Staff/Kit/AddKit.jsx
+++ src/Staff/Kit/AddKit.jsx
@@ -1,28 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap'
+import { db } from '../../Database/firebase';
+import { addDoc,collection } from 'firebase/firestore';
 
 function AddKit() {
   
-  function handlesubmit(){
+  const kitCollectionRef = collection(db, "Kit");
+  const [newKit, setKit] = useState();
+  const [newdesc, setDesc] = useState();
+  const naviagte = useNavigate();
 
+  const createKit = async (e) =>{
+    e.preventDefault();
+    try{
+      await addDoc(kitCollectionRef, {Name: newKit, Description: newdesc });
+      naviagte("/kit")
+    }
+    catch(e){
+      console.log(e.message);
+    }
   }
 
   return (
     <div className="content">
-        <h2>Kit Infomation</h2>
-        <hr />
-      <form>
+      <h2>Kit Infomation</h2>
+      <hr />
+      
+      <form onSubmit={createKit}> 
         <div className="form-group">
           <label htmlFor='KitName'>Name </label>
-          <input type="text" className="form-control" id="KitName" placeholder="Dementia Kit xx - example" required />
+          <input type="text" onChange={(event) => {
+          setKit(event.target.value);
+        }} 
+        className="form-control" 
+        id="KitName" 
+        placeholder="Dementia Kit xx - example"
+        required />
         </div>
+
         <div className="form-group">
           <label htmlFor="KitQuantity">Quantity </label>
-          <input type="number" className="form-control" id="KitQuantity" placeholder="1" required />
+          <input type="number" 
+          className="form-control" 
+          id="KitQuantity" 
+          placeholder="1" 
+          required />
         </div>
+        
         <div className="form-group">
           <label htmlFor="KitDescription">Description</label>
-          <textarea className="form-control" id="KitDescription" rows="3" placeholder='Kit Content' required ></textarea>
+          <textarea className="form-control"onChange={(event) => {
+            setDesc(event.target.value)
+          }}  
+          id="KitDescription" 
+          rows="3" 
+          placeholder='Kit Content' 
+          required >
+          </textarea>
         </div>
        
         <div className ="form-group">
@@ -30,9 +65,9 @@ function AddKit() {
           <br />
           <input type="file" className="form-control-file" id="KitPictures" />
         </div>
-        <Button onClick={handlesubmit} className='submit'>Submit</Button>
+        
+        <Button className='submit' type='submit' >Submit</Button>
         <br />
-        <Button href='/kit' className='submit'>Back</Button>
         
       </form>
     </div>
--- src/Staff/Kit/Index.jsx
+++ src/Staff/Kit/Index.jsx
@@ -1,43 +1,62 @@
-import React from 'react';
+// react
 import './Kit.css';
-
+import React, { useState, useEffect } from 'react';
 import { Table, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+// firebase
+import { db } from '../../Database/firebase';
+import { collection, getDocs } from 'firebase/firestore'
 
 function Kit() {
+  const [kit, setKit] = useState([]);
+  const kitCollectionRef = collection(db, "Kit");
+
+  useEffect(() => {
+    const getKit = async () => {
+      const data = await getDocs(kitCollectionRef)
+      console.log(data);
+      setKit(data.docs.map((doc) =>({...doc.data(), id: doc.id})));
+    };
+    getKit();
+  }, [])
+
+
   return (
     <div className='content'>
-      <h2>xx Dementia Kit </h2>
-      <p>View all the dementia Kits</p>
-      <Button href="/kit/add">Add</Button>
-      <hr></hr>
+      <div>
+        <h2>xx Dementia Kit </h2>
+        <p>View all the dementia Kits</p>
+        <Button href="/kit/add">Add</Button>
+        <hr></hr>
+      </div>
       <div className='table'>
-        <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#Id</th>
-            <th>Profile</th>
-            <th>Name</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Kit1.jpg</td>
-          <td>Dementia Kit 1 - Painting Kit</td>
-          <td>view</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Kit2.jpg</td>
-          <td>Dementia Kit 2 - Painting Kit</td>
-          <td>view</td>
-        </tr>
-    </tbody>
-  </Table>
+          <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#Id</th>
+              <th>Profile</th>
+              <th>Name</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          
+          {kit.map((user) => {
+          return( 
+            <tbody>
+              {""}
+              <tr>
+                <td>{user.id}</td>
+                <td>Kit1.jpg</td>
+                <td>{user.Name}</td>
+                <td><Link to ="/kit/{user.id}">link</Link></td>
+              </tr>
+            </tbody>
+            );
+          })}
+        </Table>
+      </div>
   </div>
-</div>
-)
-}
+)}
 
 export default Kit;
