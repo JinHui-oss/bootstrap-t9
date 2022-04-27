62419b0d44cfb555582091cebc1061b0bb041b64
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-27 14:44:56 +0800

    updated codes for the details page

--- src/Staff/Kit/Detail.jsx
+++ src/Staff/Kit/Detail.jsx
@@ -1,57 +1,76 @@
 // react
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './Kit.css';
-import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
-//import { Link } from 'react-router-dom';
 
-// firebase
+// bootstrap
+import { Button } from 'react-bootstrap';
+
+// firebase inital setup
 import { db } from '../../Database/firebase';
-import { collection, getDoc, doc } from 'firebase/firestore'
 
-function Detail() {
-   const [kit, setKit] = useState([]);
-   //const kitCollectionRef = collection(db, "Kit");
- 
-   useEffect(() => {
-     const getKit = async () => {
-      const docRef = doc(db, "Kit", "BPSaOiRxc4zqGXssQvDb");
+// firebase import function from firestore
+import { 
+  collection,
+  // getDocs,
+  getDoc,
+  doc
+} from 'firebase/firestore'
+
+export default function Detail() {
+  
+  //const [setKit] = useState([]);
+  const kitCollectionRef = collection(db, "Kit");
+  
+  
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getKit = async () => {
+      const docRef = doc(db, "Kit", id);
       const docSnap = await getDoc(docRef);
       
-      if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-      } else {
+      // check for display output
+      // console.log(docSnap);
+      
+      // check condition
+      if (docSnap.exists()) 
+      {
+        // display the output if the record exist 
+        let data =  docSnap.data();
+        console.log(data);
+        console.log(data.Name);
+      } 
+      else 
+      {
         // doc.data() will be undefined in this case
         console.log("No such document!");
       }
-      
+
+      // let data = await getDocs(kitCollectionRef)
+      // setKit(data.docs.map((doc) =>({...doc.data(), id: doc.id})));
+      // console.log(setKit())
     };
-     getKit();
-    
-   }, [])
- 
+    getKit();
+
+  // eslint-disable-next-line 
+  }, [kitCollectionRef])
    
    return (
     <div className='details-content'>
        <div className='header'>
-            <h2>Kit Details </h2>
-
-            <p>View all content inside the kit</p>
-            <hr></hr>
+          <h2>Kit Details </h2>
+          <p>View all content inside the kit</p>
+          <hr />
         </div>
-        <div className='table'>
-          <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Profile</th>
-              <th>Id</th>
-              <th>Name</th>
-            </tr>
-          </thead>
-          
-        </Table>
+        <p>User Id: <br/>{id}</p>
+      
+      {/* Button */}
+      <Button href="/Kit">Back</Button>
+      <br /> 
+      <br />
+      <Button href="#">Update</Button>
       </div>
-    </div>
   )
 }
 
-export default Detail
