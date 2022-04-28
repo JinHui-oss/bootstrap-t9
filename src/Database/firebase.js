ba77cdd83bc55b7488b3aa2662264f9b6790c5ab
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-04-28 17:37:31 +0800

    added feature of uploading files

--- src/Database/firebase.js
+++ src/Database/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,8 +20,9 @@ const firebaseConfig = {
   measurementId: "G-H2XM9JQLKY"
 };
 
-// Initialize Firebase
+// Initialize Firebase 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export default app; 
--- src/Staff/Kit/DementiaKit/Create.jsx
+++ src/Staff/Kit/DementiaKit/Create.jsx
@@ -1,29 +1,65 @@
+// CSS 
+import '../DementiaKit/Kit.css'
+
+// React
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap'
-import { db } from '../../../Database/firebase';
-import { addDoc,collection } from 'firebase/firestore';
-import '../DementiaKit/Kit.css'
+
+// Firebase initial config
+import { db, storage } from '../../../Database/firebase';
+
+// Firebase features 
+import { ref, uploadBytes } from 'firebase/storage';
+import { addDoc, collection } from 'firebase/firestore';
+
+// Random unique id 
+import { v4 } from 'uuid'
+
 
 function AddKit() {
-  
+  // create and stored the data into the firebase
   const kitCollectionRef = collection(db, "Kit");
+  
+  // retrieve the data from the user input and stored into variable
   const [newKit, setKit] = useState();
   const [newdesc, setDesc] = useState();
   const [newamt, setAmt] = useState();
-  const naviagte = useNavigate();
+  const [ImageUpload, setImageUpload] = useState(null);
+  const navigate = useNavigate();
 
   const createKit = async (e) =>{
+    // prevent the button from being spammed when there is no data
     e.preventDefault();
+    
+    //
     try{
-      await addDoc(kitCollectionRef, {Name: newKit, Description: newdesc, Quantity: newamt });
-      naviagte("/kit")
-    }
+      // check the condition if there is no photo uploaded to server
+      if(ImageUpload == null)return;
+      // set the specific path of where the photo is stored thru variable
+      const imageRef = ref(storage, `Staff/Kit/${ImageUpload.name + v4()}`)
+      // upload directly to storage database
+      uploadBytes(imageRef, ImageUpload).then(() =>{
+          alert("image upload")
+      })
+      // upload directly to cloud firestore database & return back to kit page
+      await addDoc(kitCollectionRef, 
+        {
+          Name: newKit, 
+          Description: newdesc,
+          Quantity: newamt,
+          //PhotoUrl: ImageUpload 
+        });
+      navigate("Kit")
+      }  
+    
+    // catch error messages and displayed to the users
     catch(e){
       console.log(e.message);
     }
   }
 
+  // display the form for the user to input for dementia kit
   return (
     <div className="content">
       <h2>Kit Infomation</h2>
@@ -67,10 +103,15 @@ function AddKit() {
         <div className ="form-pic">
           <label htmlFor="KitPictures">Kit Pictures</label>
           <br />
-          <input type="file" className="form-control-file" id="KitPictures" />
+          <input type="file" onChange={(event) => {
+            setImageUpload(event.target.files[0]);
+          }} className="form-control-file"  />
         </div>
         
+        {/* Submit form when user completed the form */}
         <Button className='submit' type='submit' >Submit</Button>
+        
+        {/* Return back to the kit page */}
         <Button className= 'back-action' href='/Kit'>Back</Button>
         <br />
         
