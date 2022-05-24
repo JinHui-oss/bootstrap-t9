0459c124aedb8d507fe5066134292bc0599f2e9d
Gina49-JH <99029604+GinaYuri@users.noreply.github.com>
2022-05-24 11:55:54 +0800

    added data directly to the firestore.

--- src/Login/Signup.jsx
+++ src/Login/Signup.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { UserAuth } from '../Scripts/authContext'
 import { Form, Button } from 'react-bootstrap'
+import { addDoc, collection, doc } from 'firebase/firestore'
+import { auth, db } from '../Database/firebase';
+
 
 function Signup() {
 
@@ -11,13 +14,25 @@ function Signup() {
     const [error, setError] = useState('');
 
     const {createUser} = UserAuth();
+   
     const navigate = useNavigate();
 
     const handlesubmit = async (e) =>{
         e.preventDefault();
+        let date = new Date();
+        let pass = "Member";
+
+        const kitCollectionRef = collection(db, "Member");
         setError(error)
         try{
-            await createUser(email,password)
+            await createUser(email,password,pass)
+            await addDoc(kitCollectionRef, {
+                Email: email,
+                Password: password,
+                CreatedAt: date.toDateString(),
+                PhotoUrl: "https://cdn-icons.flaticon.com/png/512/3033/premium/3033143.png?token=exp=1653291425~hmac=2a423454698479852bd5968e418fd3c3",
+                Role: pass
+            })
             navigate('/account');
         }catch(e){
             setError(e.message)
